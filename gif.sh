0f23909c0bc67ea18412d66df3a3e484a856314a
joshmyersdean <joshmyersdean@yahoo.com>
2020-03-13 23:04:19 -0700

    update shell script

--- gif.sh
+++ gif.sh
@@ -2,9 +2,9 @@
 echo "converting images to gif"
 cd WWURay
 ls -v video >> $1.txt
-cat $1.txt
 cd video
 convert -delay xx @../$1.txt ../results/$1.gif
+rm s*
 cd ..
 rm $1.txt
 echo "conversion done"
